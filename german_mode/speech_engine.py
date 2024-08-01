797f50a471811985e9e6e329e49a948a8f3f972e
markus <markus@ThinkPadMarkus.de>
2024-08-01 18:41:23 +0200

    fix: update phrase type (nimitz)
    The type of phrases changed from `Phrase` to `list[str]`.
    Though temporary german mode still worked, this threw some errors due to
    the unexpected type.

--- german_mode/speech_engine.py
+++ german_mode/speech_engine.py
@@ -21,19 +21,19 @@ speech_system.register('post:phrase', on_post_phrase)
 @mod.action_class
 class Actions:
 
-	def vosk_recognize_german(phrase: Phrase):
+	def vosk_recognize_german(phrase: list[str]):
 			"""Replay speech from last phrase into german engine"""
 			# NOTE: this is pretty much all considered an experimental API
 			# and this script is just for demo purposes, for the beta only
 			current_phrase = phrase_stack[-1]
 			ts = current_phrase['_ts']
-			start = phrase.words[0].start - ts
+			start = phrase[0].start - ts
 			# NOTE: might have to tweak this depending on engine / model if words
 			# get lost or parts of your prefix appear as word (for example getting
 			# "an ..." when saying "german ..." given prefix "german")
 			tweak_offset = -0.1 # TO DO: extract into a setting
 			start = max(0, start + tweak_offset)
-			end = phrase.words[-1].end - ts
+			end = phrase[-1].end - ts
 			samples = current_phrase['samples']
 			pstart = int(start * 16_000)
 			pend = int(end * 16_000)
@@ -44,7 +44,7 @@ class Actions:
 				speech_system._on_audio_frame(samples)
 				# Change command history entry
 				german_text = actions.user.history_get(0)
-				phrase_text = actions.user.history_transform_phrase_text(phrase.words)
+				phrase_text = actions.user.history_transform_phrase_text(phrase)
 				command = actions.user.history_get(1).removesuffix(phrase_text)
 				actions.user.history_set(1, command + german_text)
 				actions.user.history_set(0, None)
